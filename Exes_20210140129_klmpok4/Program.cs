de093ccf74b6657b707bcaa42810accf90901fc2
Raihan454 <raihanmarsa712@gmail.com>
2022-12-19 15:51:12 +0700

    Add project files.

---
+++ Exes_20210140129_klmpok4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exes_20210140129_klmpok4
+{
+    class Program
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
